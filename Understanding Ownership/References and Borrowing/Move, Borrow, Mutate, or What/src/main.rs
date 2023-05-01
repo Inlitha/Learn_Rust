275267eb8a1d83e80192a2da114d1b09d53738fa
Inlitha <of_hyrule@hotmail.fr>
2023-05-01 08:46:13 +0200

    point du 01/05/2023

--- Understanding Ownership/References and Borrowing/Move, Borrow, Mutate, or What/src/main.rs
+++ Understanding Ownership/References and Borrowing/Move, Borrow, Mutate, or What/src/main.rs
@@ -1,17 +1,17 @@
 fn main() {
-    let message = String::from("Rust is great!");
+    let mut message = String::from("Rust is great!");
 
-    let c = get_last_char(/*TODO*/);
+    let c = get_last_char(&message);
     println!("Last char is '{}'", c);
 
-    uppercase_and_print(/*TODO*/);
+    uppercase_and_print(&mut message);
 }
 
-fn get_last_char(/*TODO*/) -> char {
+fn get_last_char(msg: &String) -> char {
     msg.chars().last().unwrap()
 }
 
-fn uppercase_and_print(/*TODO*/) {
-    msg = msg.to_uppercase();
+fn uppercase_and_print(mut msg: &mut String) {
+    *msg = msg.to_uppercase();
     println!("The message is \"{}\"", msg);
 }
