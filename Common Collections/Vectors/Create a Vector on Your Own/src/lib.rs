f99a71f979be21198b58e9ea2b886df32531d742
Inlitha <of_hyrule@hotmail.fr>
2023-11-22 12:56:30 +0100

    point du 22/11/2023

--- Common Collections/Vectors/Create a Vector on Your Own/src/lib.rs
+++ Common Collections/Vectors/Create a Vector on Your Own/src/lib.rs
@@ -3,8 +3,8 @@ pub fn create_array() -> [i32; 4] {
     a
 }
 
-pub fn create_vector() -> /* TODO */ {
-    let v = /* TODO */;
+pub fn create_vector() -> Vec<i32> {
+    let v = vec![create_array()[0], create_array()[1], create_array()[2], create_array()[3]];
     v
 }
 
