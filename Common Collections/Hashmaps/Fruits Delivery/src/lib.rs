9bd09c31ad3582e82fec86195d556dc682a634a0
Inlitha <of_hyrule@hotmail.fr>
2024-02-29 13:45:48 +0100

    point du 29/02/2024

--- Common Collections/Hashmaps/Fruits Delivery/src/lib.rs
+++ Common Collections/Hashmaps/Fruits Delivery/src/lib.rs
@@ -1,11 +1,11 @@
 use std::collections::HashMap;
 
 pub fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // Declare your hash map here
+    let mut basket = HashMap::new();
 
     // Two bananas are already given to you :)
     basket.insert(String::from("banana"), 2);
-    // Put more fruits in your basket here!
-
+    basket.insert(String::from("pomme"), 3);
+    basket.insert(String::from("fraise"), 5);
     basket
 }
